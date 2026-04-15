a1ee85a8b9e07afb65a5d9bc72c496b5ca8334d6
David <mario.guerrero.marquez@uabc.edu.mx>
2026-04-14 23:16:30 -0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module uabc.david.practica3poo2026 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens uabc.david.practica3poo2026 to javafx.fxml;
+    exports uabc.david.practica3poo2026;
+}
