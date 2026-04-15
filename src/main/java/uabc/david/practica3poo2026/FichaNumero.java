b59d11583db7911e22c9ae900ea9be1121edcc35
David <mario.guerrero.marquez@uabc.edu.mx>
2026-04-14 23:46:18 -0700

    Añadidos parte de los métodos esenciales de la clase FichaNumero.

--- src/main/java/uabc/david/practica3poo2026/FichaNumero.java
+++ src/main/java/uabc/david/practica3poo2026/FichaNumero.java
@@ -41,4 +41,54 @@ public class FichaNumero {
 
     }
 
+    public Group getVista() {
+        return vista;
+    }
+
+    public void mover(int dx, int dy) {
+        this.xInicial = dx;
+        this.yInicial = dy;
+
+    }
+
+    public void mostrar() {
+
+    }
+
+    private void recalcularMedidas() {
+        tamañoDelPixel = tamañoDeLaFicha / 7;
+        bordeX = (tamañoDeLaFicha / 7) * 2;
+        bordeY = tamañoDeLaFicha / 7;
+        mediaFicha = (tamañoDeLaFicha / 14) * 6;
+    }
+
+    private void ordenarLaFicha() {
+        fondoDeLaFicha.mover(xInicial, yInicial);
+        fondoDeLaFicha.changeSize(tamañoDeLaFicha);
+
+        for (Square pixel : pixeles) {
+            pixel.changeSize(tamañoDelPixel);
+        }
+
+        pixeles.get(0).mover(xInicial + bordeX, yInicial + bordeY);
+        pixeles.get(1).mover(xInicial + mediaFicha, yInicial + bordeY);
+        pixeles.get(2).mover(xInicial + bordeX * 2, yInicial + bordeY);
+
+        pixeles.get(3).mover(xInicial + bordeX, yInicial + bordeY * 2);
+        pixeles.get(4).mover(xInicial + mediaFicha, yInicial + bordeY * 2);
+        pixeles.get(5).mover(xInicial + bordeX * 2, yInicial + bordeY * 2);
+
+        pixeles.get(6).mover(xInicial + bordeX, yInicial + bordeY * 3);
+        pixeles.get(7).mover(xInicial + mediaFicha, yInicial + bordeY * 3);
+        pixeles.get(8).mover(xInicial + bordeX * 2, yInicial + bordeY * 3);
+
+        pixeles.get(9).mover(xInicial + bordeX, yInicial + bordeY * 4);
+        pixeles.get(10).mover(xInicial + mediaFicha, yInicial + bordeY * 4);
+        pixeles.get(11).mover(xInicial + bordeX * 2, yInicial + bordeY * 4);
+
+        pixeles.get(12).mover(xInicial + bordeX, yInicial + bordeY * 5);
+        pixeles.get(13).mover(xInicial + mediaFicha, yInicial + bordeY * 5);
+        pixeles.get(14).mover(xInicial + bordeX * 2, yInicial + bordeY * 5);
+    }
+
 }
